bc17da7f45c8ad61d1ea8b9b7427256751b77a95
Praktikant <praktikant@fullsystem.local>
2019-01-10 16:48:26 +0100

    added cog load, unload, reload

--- ext/system.py
+++ ext/system.py
@@ -29,26 +29,68 @@ class System:
         """Says when a member joined."""
         await ctx.send('{0.name} joined in {0.joined_at}'.format(member))
 
+
+
+
+    # --- @bot Cog handling
+    # extention(): async
+    @commands.group(hidden = True)
+    @commands.is_owner()
+    async def system(self, ctx):
+        """manage the Bot System"""
+        if ctx.invoked_subcommand is None:
+            await ctx.send('no param...')
+
+
+    @system.command(name='load', hidden=True)
+    async def cog_load(self, ctx, *, cog: str):
+        """load a Module."""
+        self.bot.load_extension(cog)
+        return await ctx.send(f"System Module '{cog}' got successfully loaded")
+
+
+    @system.command(name='unload', hidden=True)
+    async def cog_unload(self, ctx, *, cog: str):
+        """unload a Module."""
+        self.bot.unload_extension(cog)
+        return await ctx.send(f"System Module '{cog}' got successfully unloaded")
+
+
+    @system.command(name='reload', hidden=True)
+    async def cog_reload(self, ctx, *, cog: str):
+        """reload a Module."""
+        self.bot.unload_extension(cog)
+        self.bot.load_extension(cog)
+        return await ctx.send(f"System Module '{cog}' got successfully reloaded")
+
+
+
+
     # --- @bot.event handling
     # Event: Command got fired
     async def on_command(self, ctx):
         if not isinstance(ctx.message.channel, discord.DMChannel):
             await ctx.message.delete()
 
+
     # Event: Bot joins a server (guild)
     async def on_guild_join(self, guild: discord.Guild):
         print("[System] EquinsBot joined %s [ID: %d]" % (guild.name, guild.id))
+
 
     # Event: User joins the server (guild)
     async def on_member_join(self, member: discord.Member):
         timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
         await self.bot.get_channel(self.bot.log).send("`➕` `⏱️ %s` [%s|%s] joined the server." % (timestamp, member.guild.name, member.name))
 
+
     # Event: User leaves the server (guild)
     async def on_member_remove(self, member: discord.Member):
         timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
         await self.bot.get_channel(self.bot.log).send("`➖` `⏱️ %s` [%s|%s] left the server." % (timestamp, member.guild.name, member.name))
 
+
+    # Event: Command executed completely
     async def on_command_completion(self, ctx):
         timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
         if not isinstance(ctx.message.channel, discord.DMChannel):
