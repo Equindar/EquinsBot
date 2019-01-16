e931e2cf13e6805aaab6d685e841be342e21c415
Equindar <equindar@gmail.com>
2019-01-16 13:57:47 +0100

    wipe out old stuff

--- cfg/config.py
+++ cfg/config.py
@@ -1,18 +1,2 @@
 
-# --- Configuration: Connection (Socket)
-HOST    = "irc.twitch.tv"
-PORT    = 6667
-AUTHKEY = "oauth:hd0fd8x3a1yygywkzx191jqtvhqieu"
-USER    = "equin_bot"
-
-CHANNEL = "#ghorgaz"
-
-# --- Configuration: Bot
-silent_mode     = True
-
-on_join_msg     = "I am here to serve you, my Master."
-on_leave_msg    = "It was a pleasure to serve, my Master."
-
-
-# --- Configuration: Users
-users = [ "equindar", "ghorgaz" ]
+# --- Configuration:
