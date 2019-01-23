dc705507d00bf572c9df8f017191b87caaf46652
Equindar <equindar@gmail.com>
2019-01-23 16:50:37 +0100

    added recovery routines

--- ext/dev.py
+++ ext/dev.py
@@ -8,6 +8,10 @@ from datetime import datetime
 
 class DEV:
     # --- attributs
+    staff = 519421170517540864
+    participant = 519421297235853313
+    backQueue = 521693581589741573
+
     server = {
         "Equindar": 509301003376525314,
         "NorthgardBattle": 486105985782644736
@@ -41,7 +45,6 @@ class DEV:
             'ID': s.id,
             'Owner': f"{s.owner.name} [ID: {s.owner.id}]"
         }
-
         for k,v in dict.items():
             data += f"{k}\n"
             value += f"{v}\n"
@@ -58,11 +61,9 @@ class DEV:
         """display category information"""
         name = ""
         id = ""
-
         for item in ctx.guild.categories:
             name += f"`#{item.position}` {item.name} \u200b \u200b \u200b\n"
             id += f"{item.id}\n"
-
         embed = discord.Embed(title=f"__{ctx.guild.name}__",colour=3158584)
         embed.set_footer(text="--- Category information ---")
         embed.add_field(name="Name", value=name, inline=True)
@@ -76,7 +77,6 @@ class DEV:
         """display roles information"""
         name = ""
         id = ""
-
         for item in ctx.guild.channels:
             if type == "text":
                 if isinstance(item, discord.TextChannel):
@@ -86,7 +86,6 @@ class DEV:
                 if isinstance(item, discord.VoiceChannel):
                     name += f"`#{item.position}` {item.name} \u200b \u200b \u200b\n"
                     id += f"{item.id}\n"
-
         embed = discord.Embed(title=f"__{ctx.guild.name}__",colour=3158584)
         embed.set_footer(text="--- Channel information ---")
         embed.add_field(name="Name", value=name, inline=True)
@@ -100,11 +99,9 @@ class DEV:
         """display roles information"""
         name = ""
         id = ""
-
         for item in ctx.guild.roles:
             name += "{} \u200b \u200b \u200b\n".format(item.name.replace("@","\@"))
             id += f"{item.id}\n"
-
         embed = discord.Embed(title=f"__{ctx.guild.name}__",colour=3158584)
         embed.set_footer(text="--- Role information ---")
         embed.add_field(name="Name", value=name, inline=True)
@@ -140,19 +137,142 @@ class DEV:
 #            if item.name == "Test":
 #                print(item.id)
 
-
-
-    # myroles(): async
-    # Check this:
-    # " ".join(r.name.replace("@",\@") for r in ctx.guild.roles)
+    # recover_team_channels(): async
     @commands.command(hidden=True)
     @commands.is_owner()
     @commands.guild_only()
-    async def myroles(self, ctx):
-        member = ctx.guild.get_member(ctx.message.author.id)
-        for role in member.roles:
-            if role.name != "@everyone":
-                await ctx.send(f"Role: {role.name} [ID: {role.id}]")
+    async def recover_team_channels(self, ctx):
+        # init
+        server = self.bot.get_guild(self.bot.northgardbattle)
+        for item in server.categories:
+            if item.id == 519174714002898984:
+                category = item
+        async with aiosqlite.connect('./ext/Northgard/battle/data/battle-db.sqlite') as db:
+            async with db.execute("""
+                SELECT participant.ParticipantTypeID, participant.Position, team.Name
+                FROM participant
+                LEFT JOIN team ON team.TeamID = participant.TeamID
+                WHERE participant.TournamentID = 2
+                ORDER BY participant.ParticipantID;""") as cursor:
+                async for result in cursor:
+                    team = None
+                    flag = True
+                    if team is None:
+                        for item in server.roles:
+                            if f"Team: {result[2]}" == item.name:
+                                team = item
+                                break
+                    for channel in category.channels:
+                        if f"[#{result[1]}] {result[2]}" == channel.name:
+                            flag = False
+                            break
+                    if flag:
+                        perms = {   team: discord.PermissionOverwrite(read_messages=True, connect=True, speak=True),
+                            server.get_role(self.staff): discord.PermissionOverwrite(read_messages=True, connect=True, speak=True),
+                            server.get_role(self.participant): discord.PermissionOverwrite(read_messages=False),
+                            server.get_role(self.backQueue): discord.PermissionOverwrite(read_messages=False),
+                            server.default_role: discord.PermissionOverwrite(read_messages=False, connect=False)
+                        }
+                        team_ch = await server.create_voice_channel(f"[#{result[1]}] {result[2]}", category=category, overwrites=perms)
+
+
+
+
+    # recover_roles(): async
+    @commands.command(hidden=True)
+    @commands.is_owner()
+    @commands.guild_only()
+    async def recover_roles(self, ctx):
+        # init:
+        server = self.bot.get_guild(self.bot.northgardbattle)
+
+        participant = server.get_role(519421297235853313)
+        backQueue = server.get_role(521693581589741573)
+        team_leader = server.get_role(519185793907032083)
+        team_member = server.get_role(519422680647204873)
+        team_sub = server.get_role(519433111478599690)
+
+        rec_leader = []
+        rec_member = []
+        rec_sub = []
+        rec_team = []
+        rec_part = []
+        rec_back = []
+        users = { }
+        async with aiosqlite.connect('./ext/Northgard/battle/data/battle-db.sqlite') as db:
+            # check: list active tournament
+            async with db.execute("""
+                SELECT discord.InternalID, player.Name, teamplayer.RoleID, team.Name, participant.ParticipantTypeID
+                FROM player
+                LEFT JOIN discord ON discord.ReferenceID = player.PlayerID
+                LEFT JOIN teamplayer ON teamplayer.PlayerID = player.PlayerID
+                LEFT JOIN team ON team.TeamID = teamplayer.TeamID
+                LEFT JOIN participant ON participant.TeamID = team.TeamID
+                WHERE discord.Reference = "Player"
+                ORDER BY player.PlayerID ASC;""") as cursor:
+                async for result in cursor:
+                    users[result[0]] = (result)
+        for member in server.members:
+            print(member.name)
+            if member.id in users.keys():
+
+                if team_leader not in member.roles:
+                    if users[member.id][2] == 2:
+                        rec_leader.append(member.name)
+                        await member.add_roles(team_leader)
+                if team_member not in member.roles:
+                    if users[member.id][2] == 3:
+                        rec_member.append(member.name)
+                        await member.add_roles(team_member)
+                if team_sub not in member.roles:
+                    if users[member.id][2] == 4:
+                        rec_sub.append(member.name)
+                        await member.add_roles(team_sub)
+
+                if participant not in member.roles:
+                    if users[member.id][4] == 1:
+                        rec_part.append(member.name)
+                        await member.add_roles(participant)
+                        for item in server.roles:
+                            if f"Team: {users[member.id][3]}" == item.name:
+                                team = item
+                                rec_team.append(member.name)
+                                await member.add_roles(team)
+                                break
+
+                if backQueue not in member.roles:
+                    if users[member.id][4] == 2:
+                        rec_back.append(member.name)
+                        await member.add_roles(backQueue)
+                        for item in server.roles:
+                            if f"Team: {users[member.id][3]}" == item.name:
+                                team = item
+                                rec_team.append(member.name)
+                                await member.add_roles(team)
+                                break
+
+        desc = f"`✔️` The following Users got their Roles recovered:"
+        embed = discord.Embed(title = "__Role Recovery from Database__", description=desc,colour=discord.Colour.dark_green(), timestamp = datetime.now())
+        embed.set_footer(text="--- EquinsBot --- || --- Data Recovery --- ||")
+        if len(rec_leader) != 0:
+            embed.add_field(name=f"Role: Team Leader ({len(rec_leader)})", value=", ".join(rec_leader), inline=False)
+        if len(rec_member) != 0:
+            embed.add_field(name=f"Role: Team Member ({len(rec_member)})", value=", ".join(rec_member), inline=False)
+        if len(rec_sub) != 0:
+            embed.add_field(name=f"Role: Team Substitute ({len(rec_sub)})", value=", ".join(rec_sub), inline=False)
+        if len(rec_part) != 0:
+            embed.add_field(name=f"Role: Participant ({len(rec_part)})", value=", ".join(rec_part), inline=False)
+        if len(rec_back) != 0:
+            embed.add_field(name=f"Role: BackupQueue ({len(rec_back)})", value=", ".join(rec_back), inline=False)
+        if len(rec_team) != 0:
+            embed.add_field(name=f"Team Roles ({len(rec_team)})", value=", ".join(rec_team), inline=False)
+        return await server.get_channel(537553412041080833).send(embed=embed)
+
+
+#        member = ctx.guild.get_member(ctx.message.author.id)
+#        for role in member.roles:
+#            if role.name != "@everyone":
+#                await ctx.send(f"Role: {role.name} [ID: {role.id}]")
 
 
     @commands.command(hidden=True)
